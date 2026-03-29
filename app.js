477011f1104da265626c594f5b3d7cd86094ab4e
djalil benyelles <djalil@Mac.lan>
2026-03-29 12:05:07 +0200

    Ajout du serveur https

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const session = require('express-session');
 const app = express();
 
@@ -10,7 +12,8 @@ app.use(express.static('public'));
 app.use(session({
   secret: 'postit-secret-key',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  cookie: { secure: true }
 }));
 
 // Rendre l'utilisateur connecté disponible dans toutes les vues
@@ -33,4 +36,13 @@ app.use(async (req, res, next) => {
 app.use('/', require('./routes/postits'));
 app.use('/', require('./routes/auth'));
 
-app.listen(3000, () => console.log('Serveur démarré sur http://localhost:3000'));
+// Charge le certificat et la clé privée
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+// Crée un serveur HTTPS au lieu de HTTP
+https.createServer(options, app).listen(3000, () => {
+  console.log('Serveur HTTPS démarré sur https://localhost:3000');
+});
